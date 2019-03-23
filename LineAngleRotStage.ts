4149f5d3d4fb14f557967757ff386a1de077565c
Anwesh43 <anweshthecool0@gmail.com>
2019-03-23 18:40:01 +0530

    adding LARNode class

--- LineAngleRotStage.ts
+++ LineAngleRotStage.ts
@@ -129,3 +129,48 @@ class Animator {
        }
     }
 }
+
+class LARNode {
+
+    next : LARNode
+    prev : LARNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new LARNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawLARNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LARNode {
+        var curr : LARNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr != null) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
