62d73b17476a9b288bb3732f271a9c4b61bd54bf
Anwesh43 <anweshthecool0@gmail.com>
2019-03-23 18:46:53 +0530

    using renderer in LineAngleRotStage class

--- LineAngleRotStage.ts
+++ LineAngleRotStage.ts
@@ -59,6 +59,7 @@ class LineAngleRotStage {
 
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
+    renderer : Renderer = new Renderer()
 
     initCanvas() {
         this.canvas.width = w
@@ -70,11 +71,14 @@ class LineAngleRotStage {
     render() {
         this.context.fillStyle = backColor
         this.context.fillRect(0, 0, w, h)
+        this.renderer.render(this.context)
     }
 
     handleTap() {
         this.canvas.onmousedown = () => {
-
+            this.renderer.handleTap(() => {
+                this.render()
+            })
         }
     }
 
