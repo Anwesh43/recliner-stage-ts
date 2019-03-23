d32dd256348cd0b0afe9f3f0d7627b59bd5c8015
Anwesh43 <anweshthecool0@gmail.com>
2019-03-23 18:43:26 +0530

    adding LineAngleRot class

--- LineAngleRotStage.ts
+++ LineAngleRotStage.ts
@@ -174,3 +174,27 @@ class LARNode {
         return this
     }
 }
+
+class LineAngleRot {
+
+    root : LARNode = new LARNode(0)
+    curr : LARNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
